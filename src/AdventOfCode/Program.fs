7aaf3ab851e7ba29d46c11527700bac7a6379fb0
Nicolas COUSIN <nicolas.cousin.audensiel@axa-ws.fr>
2021-12-01 14:13:17 +0100

    refactor: modifie la sortie console

--- src/AdventOfCode/Program.fs
+++ src/AdventOfCode/Program.fs
@@ -5,4 +5,5 @@ open System.IO
 
 module program =
     let depths = File.ReadAllLines("inputs/day1.txt")
-    printfn "%A" (depths |> Depths.numberOfTimesTheSumOfMeasurementsInSlidingWindow 3)
+    printfn "Day 1: the number of times a depth measurement increases is %i" (depths |> Depths.numberOfTimesADepthMeasurementIncreases)
+    printfn "Day 1: the number of times the sum of measurements in this sliding window increases %i" (depths |> Depths.numberOfTimesTheSumOfMeasurementsInSlidingWindow 3)
